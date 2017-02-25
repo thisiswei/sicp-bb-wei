4263b5aaad29f80e4016d34a50444de0c4f107d7
thisiswei <99plus2@gmail.com>
2017-02-25 12:37:23 -0500

    2.36

--- random.rkt
+++ random.rkt
@@ -1195,4 +1195,13 @@
               1))
           t)))
 
+    ; 2.36
+    (define (accumulate-n op init seqs)
+      (if (null? (car seqs))
+        null
+        (cons (my-accumulate-fold-right op init (map car seqs))
+              (accumulate-n op init (map cdr seqs)))))
+    ; (accumulate-n + 0 '('(1 2 3) '(4 5 6) '(7 8 9) '(10 11 12)))
+
+
 )
