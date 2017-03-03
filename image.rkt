6c08ca5ce8e93621afc59953123020ca736925a9
thisiswei <99plus2@gmail.com>
2017-03-03 10:23:18 -0500

    2.48

--- image.rkt
+++ image.rkt
@@ -104,4 +104,15 @@
                    (end-segment segment))))
               segment-list)))
 
+       ; 2.48
+       (define (make-segment v1 v2)
+         (cons v1 v2))
+
+       (define (start-segment seg)
+         (car seg))
+
+       (define (end-segment seg)
+         (cdr seg))
+
+
 )
